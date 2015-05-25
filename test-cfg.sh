f9b1f7b4427bedf3ab073bd1a44fb0eed56acb13
Tristan Vanderbruggen <vanderbruggentristan@gmail.com>
2015-05-25 14:15:13 -0400

    Edited test-cfg.sh to limit the number of tested graphs.

--- test-cfg.sh
+++ test-cfg.sh
@@ -1,18 +1,17 @@
 #!/bin/bash
 
+n=$1
+[ -z $n ] && n=10
+
 mkdir -p logs
 
-for li in $(cat samples-routines-num-blocks.csv | sort -g -k 3 | tr ' ' ','); do
+for li in $(cat samples-routines-num-blocks.csv | sort -g -k 3 | shuf -n $n | tr ' ' ','); do
   gi=$(echo $li | awk -F, '{print $1}')
   ri=$(echo $li | awk -F, '{print $2}')
 
-  [ $(echo $li | awk -F, '{print $4}') -lt 6 ] && continue;
-
-  for lj in $(cat samples-routines-num-blocks.csv | sort -g -k 3 | tr ' ' ','); do
+  for lj in $(cat samples-routines-num-blocks.csv | sort -g -k 3 | shuf -n $n | tr ' ' ','); do
     gj=$(echo $lj | awk -F, '{print $1}')
     rj=$(echo $lj | awk -F, '{print $2}')
-
-    [ $(echo $lj | awk -F, '{print $4}') -lt 6 ] && continue;
 
     ./test-cfg samples/$gi/$gi-$ri.json samples/$gj/$gj-$rj.json > logs/$gi-$ri-$gj-$rj.log
     cat logs/$gi-$ri-$gj-$rj.log
